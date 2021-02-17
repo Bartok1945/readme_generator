9a594505c98f103beccad2ed98b54d11661a8876
Bartok <thom.huenger@gmail.com>
2021-02-17 13:16:32 -0500

    deleted readme

--- index.js
+++ index.js
@@ -18,49 +18,49 @@ const inquirerPrompts = [
         message: "What is the poject's title? ",
         name: "title",
         validate: confirmInput,
-      },
-      {
+    },
+    {
         type: "input",
         message: "Provide a description of the project: ",
         name: "description",
         validate: confirmInput,
-      },
-      {
+    },
+    {
         type: "input",
         message: "How would a user install this project? ",
         name: "installation",
-      },
-      {
+    },
+    {
         type: "input",
         message: "Describe and give examples of usage: ",
         name: "usage",
-      },
-      {
+    },
+    {
         type: "list",
         message: "Choose a license for this project: ",
         name: "license",
         choices: ["Apache", "GNU", "MIT", "BSD", "Eclipse", "Mozilla", "Unlicense"],
-      },
-      {
+    },
+    {
         type: "input",
         message: "List any collaborators and share their github links: ",
         name: "contributing",
-      },
-      {
+    },
+    {
         type: "input",
         message: "If tests are available, provide an example on how to run them: ",
         name: "tests",
-      },
-      {
+    },
+    {
         type: "input",
         message: "Enter your GitHub username: ",
         name: "github",
-      },
-      {
+    },
+    {
         type: "input",
         message: "Enter your email address: ",
         name: "email",
-      },
+    },
 ];
 
 //initiate the inquirer prompts
@@ -68,25 +68,25 @@ const inquirerPrompts = [
 const startProgram = () =>
     inquirer.prompt(inquirerPrompts).then((response) => {
         fs.writeFile("README.md", writeFile(response), (err) =>
-        err ? console.log(err) : console.log("README file generated)")
+            err ? console.log(err) : console.log("README file generated)")
         );
     });
 
-    // generate file
+// generate file
 
-    const writeFile = ({
-        title,
-  description,
-  installation,
-  usage,
-  license,
-  contributing,
-  tests,
-  github,
-  email,
-    }) =>
+const writeFile = ({
+    title,
+    description,
+    installation,
+    usage,
+    license,
+    contributing,
+    tests,
+    github,
+    email,
+}) =>
 
- `# Title
+    `# Title
 ${title}
 ![badge](https://img.shields.io/static/v1?label=license&message=${license}&color=green)
 ## Description
